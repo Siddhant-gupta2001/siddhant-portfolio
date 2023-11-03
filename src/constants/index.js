178bcd8586fe43637fc112fd1f57e22a5aefa3e1
Siddhant <siddhantgupta200103@gmail.com>
2023-11-04 03:40:22 +0530

    adding some statements

--- src/constants/index.js
+++ src/constants/index.js
@@ -172,23 +172,23 @@ import {
   const testimonials = [
     {
       testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+        "As experienced person you greatly contributed to my project's success, making it ideal for guiding newcomers.",
       name: "Sara Lee",
       designation: "CFO",
-      company: "Acme Co",
+      company: "Kublit",
       image: "https://randomuser.me/api/portraits/women/4.jpg",
     },
     {
       testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+        "I've never met a web developer who truly cares about their clients' success.",
       name: "Chris Brown",
       designation: "COO",
-      company: "DEF Corp",
+      company: "Zulip",
       image: "https://randomuser.me/api/portraits/men/5.jpg",
     },
     {
       testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+        "After  optimized our website, our traffic will be decreased 50 %.",
       name: "Lisa Wang",
       designation: "CTO",
       company: "456 Enterprises",
